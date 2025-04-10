c240732c2de840d0bb4708e77b21ac65bf414e45
makasinui <mks.shi@ya.ru>
2025-04-10 13:34:45 +0300

    feat(exchangeList): add exchange list

---
+++ src/components/exchangeList/ExchangeList.vue
@@ -0,0 +1,25 @@
+<template>
+    <h1 class="text-2xl">Курс валют</h1>
+    <div class="flex gap-2 mt-2">
+        <ExchangeInfo
+            v-for="currency in currencyToShow"
+            :to="currentLang.name"
+            :from="currency.name"
+            price="200"
+        />
+    </div>
+</template>
+
+<script lang="ts" setup>
+import { ExchangeInfo } from '@/components/ui';
+import { useLangStore } from '@/store/langStore';
+import { storeToRefs } from 'pinia';
+import { computed } from 'vue';
+
+const langStore = useLangStore();
+
+const { currentLang } = storeToRefs(langStore);
+const { langs } = langStore;
+
+const currencyToShow = computed(() => langs.filter((lang) => lang.id !== currentLang.value.id));
+</script>
--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -1,3 +1,10 @@
 <template>
-    <div>asd</div>
+    <main class="mx-4 mt-14">
+        <ExchangeList />
+    </main>
 </template>
+
+<script lang="ts" setup>
+import ExchangeList from '@/components/exchangeList/ExchangeList.vue';
+
+</script>
