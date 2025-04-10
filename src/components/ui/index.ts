86b317e28eae55c31a212822238cfc082a496afa
makasinui <mks.shi@ya.ru>
2025-04-10 14:10:45 +0300

    feat(input): add input component

--- src/components/ui/index.ts
+++ src/components/ui/index.ts
@@ -1,7 +1,9 @@
 import Dropdown from "./dropdown/Dropdown.vue";
 import ExchangeInfo from "./exchangeInfo/ExchangeInfo.vue";
+import Input from "./input/Input.vue";
 
 export {
     Dropdown,
-    ExchangeInfo
+    ExchangeInfo,
+    Input
 }
---
+++ src/components/ui/input/Input.vue
@@ -0,0 +1,22 @@
+<template>
+    <label class="w-full flex flex-col">
+        <span>{{ label }}</span>
+        <input
+            type="text"
+            :placeholder="placeholder"
+            v-model="modelValue"
+            class="py-2 px-4 border rounded-lg"
+        />
+    </label>
+</template>
+
+<script lang="ts" setup>
+interface IProps {
+    placeholder?: string;
+    label?: string
+}
+
+defineProps<IProps>();
+
+const modelValue = defineModel<string>();
+</script>
