f85a97f9026d431dacc36d24362be990431dbe8f
makasinui <mks.shi@ya.ru>
2025-04-10 12:28:30 +0300

    feat: path resolve, router home page

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -1,3 +1,3 @@
 <template>
-    <div></div>
+    <div>asd</div>
 </template>
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,7 +1,13 @@
-import { createWebHistory } from "vue-router";
+import { createRouter, createWebHistory } from "vue-router";
 import Home from "@/pages/home/Home.vue";
 
 const routes = [
     { path: '/', component: Home },
+];
 
-]
+const router = createRouter({
+    history: createWebHistory(),
+    routes
+});
+
+export default router;
